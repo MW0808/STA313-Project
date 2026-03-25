00ee1709afcdf31761b5f4a106aedfd6d25a1368
naraeleee <nar.lee@mail.utoronto.ca>
2026-03-25 15:57:38 -0400

    Placeholder for each scroll

--- sta313/src/App.js
+++ sta313/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const sections = [
+  "Landing Page",
+  "1st Scroll",
+  "2nd Scroll",
+  "3rd Scroll",
+  "4th Scroll",
+  "5th Scroll",
+  "6th Scroll",
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {sections.map((title, index) => (
+        <section className="panel" key={index}>
+          <h1>{title}</h1>
+        </section>
+      ))}
     </div>
   );
 }
